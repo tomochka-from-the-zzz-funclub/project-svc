4b834e4754267a8b274bc9bb0417299ac387b518
Ermakov Yaroslav <pengven357@gmail.com>
2024-11-19 02:06:12 +0300

    returned arguments type ORM

--- app/domain/repositories/FilmRepository.py
+++ app/domain/repositories/FilmRepository.py
@@ -11,18 +11,19 @@ class FilmRepository:
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def add_film(self, film: Film) -> Film:
+    async def add_film(self, film: Film) -> FilmORM:
         """
         Создание нового фильма в базе данных.
 
         :param film: Объект Film, содержащий данные о фильме.
-        :return: Объект Film, представляющий созданный фильм.
+        :return: Объект FilmORM, представляющий созданный фильм.
         """
         try:
             existing_film = await self.get_film_by_title(film.title)
             if existing_film:
                 return existing_film
 
+            # Создание фильма
             film_orm = FilmORM(
                 title=film.title,
                 description=film.description,
@@ -34,129 +35,104 @@ class FilmRepository:
             await self.session.commit()
             await self.session.refresh(film_orm)
 
-            return Film(
-                id=film_orm.id,
-                title=film_orm.title,
-                description=film_orm.description,
-                creation_date=film_orm.creation_date,
-                file_link=film_orm.file_link,
-            )
+            return film_orm
 
         except IntegrityError:
             await self.session.rollback()
             raise ValueError("Не удалось добавить жанр из-за конфликта данных.")
 
 
-    async def get_film_by_id(self, film_id: int) -> Optional[Film]:
+    async def get_film_by_id(self, film_id: int) -> Optional[FilmORM]:
         """
         Получает фильм по ID.
 
-        :param film_id: ID фильма, который нужно найти
-        :return: Объект Film
-        :raises ValueError: Если фильм с указанным названием не найден.
+        :param film_id: ID фильма, который нужно найти.
+        :return: FilmORM — объект фильма, если найден, или None.
+        :raises ValueError: Если фильм с указанным ID не найден.
         """
         result = await self.session.execute(select(FilmORM).filter_by(id=film_id))
         film_orm = result.scalars().first()
 
         if film_orm:
-            return Film(
-                id=film_orm.id,
-                title=film_orm.title,
-                description=film_orm.description,
-                creation_date=film_orm.creation_date,
-                file_link=film_orm.file_link
-            )
+            return film_orm
 
         raise ValueError(
-            f"Фильм с названием '{film_id}' не найден. Проверьте правильность названия или добавьте новый фильм."
+            f"Фильм с ID '{film_id}' не найден. Проверьте правильность ID или добавьте новый фильм."
         )
 
-    async def get_films_by_ids(self, film_ids: List[int]) -> List[Film]:
+    async def get_films_by_ids(self, film_ids: List[int]) -> List[FilmORM]:
         """
         Получает фильмы по их ID, используя метод get_film_by_id.
 
         :param film_ids: List[int] — Список ID фильмов.
-        :return: List[Film] — Список объектов фильмов.
+        :return: List[FilmORM] — Список объектов FilmORM.
         """
         return [await self.get_film_by_id(film_id) for film_id in film_ids]
 
-
-    async def get_all_films(self) -> List[Film]:
+    async def get_all_films(self) -> List[FilmORM]:
         """
         Получает все фильмы из базы данных.
 
-        :return: Список объектов Film
+        :return: Список объектов FilmORM.
         """
         result = await self.session.execute(select(FilmORM))
-        films = result.scalars().all()
+        films = list(result.scalars().all())
 
-        return [Film(
-            id=film.id,
-            title=film.title,
-            description=film.description,
-            creation_date=film.creation_date,
-            file_link=film.file_link
-        ) for film in films]
+        return films
 
-    async def get_film_by_title(self, title: str) -> Film:
+    async def get_film_by_title(self, title: str) -> Optional[FilmORM]:
         """
         Получение фильма по названию.
 
         :param title: str — Название фильма.
-        :return: Film — объект фильма, если найден.
+        :return: FilmORM — объект фильма, если найден.
         :raises ValueError: Если фильм с указанным названием не найден.
         """
         result = await self.session.execute(select(FilmORM).filter_by(title=title))
         film_orm = result.scalars().first()
 
-        if not film_orm:
-            return Film(
-                id=film_orm.id,
-                title=film_orm.title,
-                description=film_orm.description,
-                creation_date=film_orm.creation_date,
-                file_link=film_orm.file_link
-            )
+        if film_orm:
+            return film_orm
 
         raise ValueError(
             f"Фильм с названием '{title}' не найден. Проверьте правильность названия или добавьте новый фильм."
         )
 
-    async def update_film(self, film_id: int, updated_film: Film) -> Optional[Film]:
+    async def update_film(self, film_id: int, updated_film: Film) -> Optional[FilmORM]:
         """
         Обновляет данные о фильме.
 
-        :param film_id: ID фильма, который нужно обновить
-        :param updated_film: Обновленный объект Film
-        :return: Обновленный объект Film, если фильм найден и обновлен, или None
+        :param film_id: ID фильма, который нужно обновить.
+        :param updated_film: Обновленный объект Film.
+        :return: Обновленный объект FilmORM, если фильм найден и обновлен, или None.
         """
-        film = await self.get_film_by_id(film_id)
+        film_orm = await self.get_film_by_id(film_id)
 
-        if film:
-            film.title = updated_film.title
-            film.description = updated_film.description
-            film.creation_date = updated_film.creation_date
-            film.file_link = updated_film.file_link
+        if film_orm:
+            film_orm.title = updated_film.title
+            film_orm.description = updated_film.description
+            film_orm.creation_date = updated_film.creation_date
+            film_orm.file_link = updated_film.file_link
 
             await self.session.commit()
 
-            return film
+            return film_orm
 
         return None
 
-    async def delete_film(self, film_id: int) -> Optional[Film]:
+    async def delete_film(self, film_id: int) -> Optional[FilmORM]:
         """
         Удаляет фильм по ID.
 
-        :param film_id: ID фильма, который нужно удалить
-        :return: Удаленный объект Film, если фильм найден и удален, или None
+        :param film_id: ID фильма, который нужно удалить.
+        :return: Удаленный объект FilmORM, если фильм найден и удален, или None.
         """
-        film = await self.get_film_by_id(film_id)
+        film_orm = await self.get_film_by_id(film_id)
 
-        if film:
-            await self.session.delete(film)
+        if film_orm:
+            await self.session.delete(film_orm)
             await self.session.commit()
 
-            return film
+            return film_orm
 
         return None
--- app/domain/repositories/GenreRepository.py
+++ app/domain/repositories/GenreRepository.py
@@ -3,20 +3,20 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from typing import List, Optional
 
-from app.domain.models.Genre import Genre
 from app.infrastructure.db.models.GenreORM import GenreORM
+from app.domain.models.Genre import Genre
 
 
 class GenreRepository:
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def add_genre(self, genre: Genre) -> Genre:
+    async def add_genre(self, genre: Genre) -> GenreORM:
         """
         Добавление нового жанра в базу данных, если он еще не существует.
 
         :param genre: Genre — объект жанра для добавления.
-        :return: Genre — объект жанра с присвоенным ID (существующий или новый).
+        :return: GenreORM — объект жанра с присвоенным ID.
         """
         try:
             existing_genre = await self.get_genre_by_name(genre.name)
@@ -24,30 +24,30 @@ class GenreRepository:
                 return existing_genre
 
             genre_orm = GenreORM(name=genre.name)
-            self.session.add(genre_orm)
 
+            self.session.add(genre_orm)
             await self.session.commit()
             await self.session.refresh(genre_orm)
 
-            return Genre(id=genre_orm.id, name=genre_orm.name)
+            return genre_orm
 
         except IntegrityError:
             await self.session.rollback()
             raise ValueError("Не удалось добавить жанр из-за конфликта данных.")
 
-    async def get_genre_by_id(self, genre_id: int) -> Optional[Genre]:
+    async def get_genre_by_id(self, genre_id: int) -> Optional[GenreORM]:
         """
         Получение жанра по ID.
 
         :param genre_id: int — ID жанра.
-        :return: Genre или None — объект жанра, если найден, иначе None.
+        :return: GenreORM или None — объект жанра, если найден, иначе None.
         :raises ValueError: Если жанр с указанным ID не найден.
         """
         result = await self.session.execute(select(GenreORM).filter_by(id=genre_id))
         genre_orm = result.scalars().first()
 
         if genre_orm:
-            return Genre(id=genre_orm.id, name=genre_orm.name)
+            return genre_orm
 
         raise ValueError(
             f"Жанр с ID {genre_id} не найден в базе данных. "
@@ -55,73 +55,72 @@ class GenreRepository:
             f"Если жанр отсутствует, его нужно создать перед использованием."
         )
 
-    async def get_genres_by_ids(self, genre_ids: List[int]) -> List[Genre]:
+    async def get_genres_by_ids(self, genre_ids: List[int]) -> List[GenreORM]:
         """
         Получает жанры по их ID, используя метод get_genre_by_id.
 
         :param genre_ids: Список ID жанров.
-        :return: Список объектов Genre.
+        :return: Список объектов GenreORM.
         """
         return [await self.get_genre_by_id(genre_id) for genre_id in genre_ids]
 
-    async def get_genre_by_name(self, name: str) -> Optional[Genre]:
+    async def get_genre_by_name(self, name: str) -> Optional[GenreORM]:
         """
         Получение жанра по названию.
 
         :param name: str — Название жанра.
-        :return: Genre или None — объект жанра, если найден, или None.
+        :return: GenreORM или None — объект жанра, если найден, или None.
         """
         result = await self.session.execute(select(GenreORM).filter_by(name=name))
         genre_orm = result.scalars().first()
 
         if genre_orm:
-            return Genre(id=genre_orm.id, name=genre_orm.name)
+            return genre_orm
 
         return None
 
-    async def get_all_genres(self) -> List[Genre]:
+    async def get_all_genres(self) -> List[GenreORM]:
         """
         Получение всех жанров.
 
-        :return: List[Genre] — список объектов жанров.
+        :return: List[GenreORM] — список объектов жанров.
         """
         result = await self.session.execute(select(GenreORM))
-        genres_orm = result.scalars().all()
+        genres_orm = list(result.scalars().all())
 
-        return [Genre(id=genre.id, name=genre.name) for genre in genres_orm]
+        return genres_orm
 
-    async def update_genre(self, genre_id: int, updated_genre: Genre) -> Optional[Genre]:
+    async def update_genre(self, genre_id: int, updated_genre: Genre) -> Optional[GenreORM]:
         """
         Обновление информации о жанре по ID.
 
         :param genre_id: int — ID жанра для обновления.
         :param updated_genre: Genre — новый объект жанра.
-        :return: Genre или None — обновлённый объект жанра или None, если не найден.
+        :return: GenreORM или None — обновлённый объект GenreORM или None, если не найден.
         """
-        genre = await self.get_genre_by_id(genre_id)
+        genre_orm = await self.get_genre_by_id(genre_id)
 
-        if genre:
-            genre.name = updated_genre.name
-
+        if genre_orm:
+            genre_orm.name = updated_genre.name
             await self.session.commit()
 
-            return genre
+            return genre_orm
 
         return None
 
-    async def delete_genre(self, genre_id: int) -> Optional[Genre]:
+    async def delete_genre(self, genre_id: int) -> Optional[GenreORM]:
         """
         Удаление жанра по ID.
 
         :param genre_id: int — ID жанра для удаления.
-        :return: Genre или None — удалённый объект жанра или None, если не найден.
+        :return: GenreORM или None — удалённый объект GenreORM или None, если не найден.
         """
-        genre = await self.get_genre_by_id(genre_id)
+        genre_orm = await self.get_genre_by_id(genre_id)
 
-        if genre:
-            await self.session.delete(genre)
+        if genre_orm:
+            await self.session.delete(genre_orm)
             await self.session.commit()
 
-            return genre
+            return genre_orm
 
         return None
