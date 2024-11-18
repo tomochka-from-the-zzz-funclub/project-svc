e2ca82fa7e589c59a5edf6385db3a3145fa123ed
Ermakov Yaroslav <pengven357@gmail.com>
2024-11-19 02:07:43 +0300

    added methods: print, to_string

--- app/domain/models/Film.py
+++ app/domain/models/Film.py
@@ -2,6 +2,9 @@ from dataclasses import dataclass
 from datetime import date
 from typing import Optional
 
+from app.domain.models.Genre import Genre
+
+
 @dataclass
 class Film:
     id: Optional[int] = None
@@ -9,3 +12,30 @@ class Film:
     description: Optional[str] = None
     creation_date: Optional[date] = None
     file_link: Optional[str] = None
+    genres: Optional[list[Genre]] = None
+
+    def __repr__(self) -> str:
+        """
+        Форматированный вывод для отладки.
+        """
+        genres = ", ".join(genre.name for genre in self.genres) if self.genres else "None"
+        return (
+            f"<Film ID={self.id}, Title='{self.title}', "
+            f"Description='{self.description or 'None'}', "
+            f"CreationDate='{self.creation_date or 'None'}', "
+            f"FileLink='{self.file_link or 'None'}', Genres=[{genres}]>"
+        )
+
+    def __str__(self) -> str:
+        """
+        Упрощённый вывод для отображения в пользовательском интерфейсе.
+        """
+        genres = ", ".join(genre.name for genre in self.genres) if self.genres else "Нет жанров"
+        return (
+            f"Фильм: {self.title}\n"
+            f"Описание: {self.description or 'Нет описания'}\n"
+            f"Дата выпуска: {self.creation_date or 'Не указана'}\n"
+            f"Ссылка: {self.file_link or 'Нет ссылки'}\n"
+            f"Жанры: {genres}"
+        )
+
--- app/domain/models/Genre.py
+++ app/domain/models/Genre.py
@@ -5,3 +5,16 @@ from typing import Optional
 class Genre:
     id: Optional[int] = None
     name: str = ""
+
+    def __repr__(self) -> str:
+        """
+        Форматированный вывод информации о жанре через repr.
+        """
+        return f"<Genre ID={self.id}, Name='{self.name}'>"
+
+    def __str__(self) -> str:
+        """
+        Форматированный вывод информации о жанре.
+        """
+        return f"Жанр ID: {self.id}, Название: {self.name}"
+
