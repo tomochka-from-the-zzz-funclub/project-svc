70dc5bddbfc29a8be591bd9afacc041c17a46fb8
Ermakov Yaroslav <pengven357@gmail.com>
2024-11-19 02:08:54 +0300

    added methods for add genres for film

--- app/use_cases/FilmService.py
+++ app/use_cases/FilmService.py
@@ -1,43 +1,60 @@
+from datetime import date
 from typing import List
+
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.domain.models.Film import Film
 from app.domain.models.Genre import Genre
+
 from app.domain.repositories.FilmRepository import FilmRepository
 from app.domain.repositories.GenreRepository import GenreRepository
+
 from app.infrastructure.db.models.FilmORM import FilmORM
 from app.infrastructure.db.models.GenreORM import GenreORM
 
 
 class FilmService:
-    def __init__(self, film_repo: FilmRepository, genre_repo: GenreRepository):
-        self.film_repo = film_repo
-        self.genre_repo = genre_repo
+    def __init__(self, session: AsyncSession):
+        self.session = session
+        self.film_repository = FilmRepository(session)
+        self.genre_repository = GenreRepository(session)
 
-    async def create_film(self, title: str, description: str, creation_date, file_link: str) -> Film:
-        film = Film(
-            title=title,
-            description=description,
-            creation_date=creation_date,
-            file_link=file_link
+    async def add_film_with_genres(self, film: Film, genre_ids: List[int]) -> Film:
+        """
+        Создание фильма с привязкой к жанрам.
+
+        :param film: Объект Film, содержащий данные о фильме.
+        :param genre_ids: Список ID жанров для привязки к фильму.
+        :return: Объект Film с привязанными жанрами.
+        """
+
+        film_orm = await self.film_repository.add_film(film)
+
+        genres = await self.genre_repository.get_genres_by_ids(genre_ids)
+
+        film_orm = await self.assign_genres_to_film(film_orm, genres)
+
+        return Film(
+            id=film_orm.id,
+            title=film_orm.title,
+            description=film_orm.description,
+            creation_date=film_orm.creation_date,
+            file_link=film_orm.file_link,
+            genres=[Genre(id=g.id, name=g.name) for g in film_orm.genres],
         )
-        return await self.film_repo.add_film(film)
 
-    async def get_all_films(self) -> List[Film]:
-        return await self.film_repo.get_all_films()
+    async def assign_genres_to_film(self, film_orm: FilmORM, genres: List[GenreORM]) -> FilmORM:
+        """
+        Присваивает жанры фильму.
 
-    async def add_genre_to_film(self, film_id: int, genre_id: int):
-        await self.validate_film_and_genre(film_id, genre_id)
+        :param film_orm: Объект ORM фильма, к которому нужно привязать жанры.
+        :param genres: Список объектов ORM жанров.
+        :return: Обновленный объект ORM фильма.
+        """
+        for genre in genres:
+            film_orm.genres.append(genre)
 
-        film_orm = await self.film_repo.session.get(FilmORM, film_id)
-        genre_orm = await self.genre_repo.session.get(GenreORM, genre_id)
-        film_orm.genres.append(genre_orm)
-        await self.film_repo.session.commit()
+        await self.session.commit()
+        await self.session.refresh(film_orm)
 
-    async def validate_film_and_genre(self, film_id, genre_id):
-        film = await self.film_repo.get_film_by_id(film_id)
-        genre = await self.genre_repo.get_genre_by_id(genre_id)
-        if not film or not genre:
-            raise ValueError("Film or Genre not found")
-
-    async def get_all_genres(self) -> List[Genre]:
-        return await self.genre_repo.get_all_genres()
+        return film_orm
